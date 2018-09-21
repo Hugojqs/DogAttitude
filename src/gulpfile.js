05a969b0ce10b166b41d0b423c40d38a742163dc
hugojqs <hugo.jqs@outlook.com>
2018-09-21 12:29:49 +0200

    init: Initialisation du squellette PUG

--- src/gulpfile.js
+++ src/gulpfile.js
@@ -1,73 +1,76 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var pug = require('gulp-pug');
+var gulp = require("gulp");
+var sass = require("gulp-sass");
+var pug = require("gulp-pug");
 
-var uglify = require('gulp-uglify');
-var connect = require('gulp-connect');
-
-
+var uglify = require("gulp-uglify");
+var connect = require("gulp-connect");
 
 // Compilation HTML -- .pug to .html
-gulp.task('build', () => {
-    return gulp.src('views/**.pug')
+gulp.task("build", () => {
+  return gulp
+    .src("views/**.pug")
     .pipe(pug())
-    .pipe(gulp.dest('dist'))
-})
+    .pipe(gulp.dest("dist"));
+});
 
 // Compilation CSS -- .scss to .css
-gulp.task('sass', () => {
-    return gulp.src('sass/**.scss')
-    .pipe(sass({style: 'minified'}))
-    .pipe(gulp.dest('dist/css'))
-})
+gulp.task("sass", () => {
+  return gulp
+    .src("sass/**.scss")
+    .pipe(sass({ style: "minified" }))
+    .pipe(gulp.dest("dist/css"));
+});
 
 // Compilation JS -- .js
-gulp.task('js', () => {
-    return gulp.src('js/**.js')
+gulp.task("js", () => {
+  return gulp
+    .src("js/**.js")
     .pipe(uglify())
-    .pipe(gulp.dest('dist/js'))
-})
-
+    .pipe(gulp.dest("dist/js"));
+});
 
 // Importer Bootstrap
-gulp.task('imp-bootstrap', () => {
-    return gulp.src([
-        "node_modules/bootstrap/dist/css/bootstrap.min.css",
-        "node_modules/bootstrap/dist/js/bootstrap.min.js"
+gulp.task("imp-bootstrap", () => {
+  return gulp
+    .src([
+      "node_modules/bootstrap/dist/css/bootstrap.min.css",
+      "node_modules/bootstrap/dist/js/bootstrap.min.js"
     ])
-    .pipe(gulp.dest('dist/vendors/bootstrap'))
-})
+    .pipe(gulp.dest("dist/vendors/bootstrap"));
+});
 
 // Importer Jquery
-gulp.task('imp-jquery', () => {
-    return gulp.src([
-        "node_modules/jquery/dist/jquery.min.js",
-    ])
-    .pipe(gulp.dest('dist/vendors/jquery'))
-})
-
+gulp.task("imp-jquery", () => {
+  return gulp
+    .src(["node_modules/jquery/dist/jquery.min.js"])
+    .pipe(gulp.dest("dist/vendors/jquery"));
+});
 
 // Live reload and watcher -- localhost:8080
-gulp.task('livereload', function (){
-    gulp.src('./dist/**/*')
-    .pipe(connect.reload());
+gulp.task("livereload", function() {
+  gulp.src("./dist/**/*").pipe(connect.reload());
+});
+
+gulp.task("watch", function() {
+  gulp.watch("./sass/**/*.scss", ["sass"]);
+  gulp.watch("./dist/**/*", ["livereload"]);
+  gulp.watch("./js/*.js", ["js"]);
+  gulp.watch("./views/**/*.pug", ["build"]);
+});
+
+gulp.task("connect", function() {
+  connect.server({
+    root: "dist",
+    livereload: true
   });
-  
-  gulp.task('watch', function () {
-    gulp.watch('./sass/**/*.scss', ['sass']);
-    gulp.watch('./dist/**/*', ['livereload']);
-    gulp.watch('./js/*.js', ['js']);
-    gulp.watch('./views/*.pug', ['build']);
-  });
+});
 
-
-  gulp.task('connect', function(){
-    connect.server({
-      root: 'dist',
-      livereload: true
-    });
-  });
-
-
-
-  gulp.task('default', ['connect', 'watch' , 'sass', 'js', 'imp-bootstrap', 'imp-jquery', 'build'])
+gulp.task("default", [
+  "connect",
+  "watch",
+  "sass",
+  "js",
+  "imp-bootstrap",
+  "imp-jquery",
+  "build"
+]);
